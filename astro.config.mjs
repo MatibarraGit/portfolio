17c7e78208aaa908349f7ef3fb23b784a6ecb05d
Mat <matiaibarr43@gmail.com>
2026-08-27 15:25:34 -0300

    Descargué y cargué las fuentes variables en el global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      name: "Jet Brains Mono",
+      cssVariable: "--font-jet-brains",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/jetbrains-mono-latin-wght-normal.woff2"],
+            weight: "100 800",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      name: "Space Grotesk",
+      cssVariable: "--font-grotesk",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/space-grotesk-latin-wght-normal.woff2"],
+            weight: "300 700",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
